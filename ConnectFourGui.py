83e38e9a04522adcc84f5e3b82cc40beb40cb551
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2019-05-16 22:13:13 +0200

    Update ConnectFourGui.py
    Adding new modes:
    Human vs Human
    Human vs Computer
    Computer vs Computer

--- ConnectFourGui.py
+++ ConnectFourGui.py
@@ -1,5 +1,9 @@
 import tkinter as tk
+from tkinter import messagebox
 from ConnectFour import ConnectFour
+import math
+import random
+import sys
 
 NROWS = 6
 NCOL = 7
@@ -7,54 +11,78 @@ NCOL = 7
 HEIGHT = 600
 WIDTH = 700
 
-playerNamesWindow = tk.Tk()
 playingWindow = tk.Tk()
+playingModeWindow = tk.Tk()
+
 
 playingCanvas = None
+connectFour = None
 
-player1NameEntry = tk.Entry(playerNamesWindow)
-player2NameEntry = tk.Entry(playerNamesWindow)
+playingMode = ""
 
-player1Name = ""
-player2Name = ""
+player1Name = "p1"
+player2Name = "p2"
 
-turn = "player1" #It can be "player1", "computer"
+playing = False
+
+piecesColor = ["red", "yellow"]
+
+turn = "" #It can be "Human", "Computer"
+playerMark = 1
 
 
 def init():
-    playerNamesWindow.title("Connect four")
-    tk.Label(playerNamesWindow, text="First player name:").grid(row=0)
-    tk.Label(playerNamesWindow, text="Second player name:").grid(row=1)
+    playingModeWindow.title("Connect four")
 
-    tk.Button(playerNamesWindow, text="Start", command=startPressed).grid(row=2)
+    tk.Label(playingModeWindow, text="Choose playing mode").grid(row=0)
 
-    player1NameEntry.grid(row=0, column=1)
-    player2NameEntry.grid(row=1, column=1)
+    tk.Button(playingModeWindow, text="Human vs Human", command=HvsH).grid(row=1)
+    tk.Button(playingModeWindow, text="Human vs Computer", command=HvsC).grid(row=2)
+    tk.Button(playingModeWindow, text="Computer vs Computer", command=CvsC).grid(row=3)
 
     playingWindow.title("Connect four")
     playingWindow.withdraw()
 
-    playerNamesWindow.mainloop()
+    playingModeWindow.mainloop()
 
-def startPressed():
-    #Get players names
-    global player1Name
-    global player2Name
-    player1Name = player1NameEntry.get()
-    player2Name = player2NameEntry.get()
+def HvsH():
+    global playingMode
+    global turn
+    playingMode = "HvsH"
+    turn = "Human"
+    start()
 
+def HvsC():
+    global playingMode
+    global turn
+    playingMode = "HvsC"
+    temp = ["Human", "Computer"]
+    turn = random.choice(temp)
+    start()
+
+def CvsC():
+    global playingMode
+    global turn
+    playingMode = "CvsC"
+    turn = "Computer"
+    start()
+
+def start():
     #Close player names window
-    playerNamesWindow.destroy()
+    playingModeWindow.withdraw()
     
     #Open the main playing window
     playingWindow.deiconify()
     playingWindow.focus()
+    drawBoard()
     playingLoop()
 
-def playingLoop():
+def drawBoard():
     global playingCanvas
     playingCanvas = tk.Canvas(playingWindow, bg='blue', height=HEIGHT, width=WIDTH)
     
+    global connectFour
+    connectFour = ConnectFour(player1Name, player2Name)
 
     for i in range(NROWS):
         for j in range(NCOL):
@@ -65,18 +93,55 @@ def playingLoop():
             playingCanvas.create_rectangle(x0, y0, x1, y1, width=0)
             playingCanvas.create_oval(x0+4, y0+4, x1-4, y1-4, fill='white', tags=(str(j)+","+str(i)))
 
+    playingCanvas.create_text(WIDTH/2, HEIGHT/2, tags="thinkingLabel", state=tk.HIDDEN,  font=('Helvetica', 60, 'bold'))
+
     playingCanvas.bind('<Button-1>', mousePressed)
 
     playingCanvas.pack()
-    playingWindow.mainloop()
+
+def playingLoop():
+    global turn
+    global playerMark
+    global playing
+    playing = True
+    while playing:
+        if turn == "Computer":
+            labelTag = playingCanvas.find_withtag("thinkingLabel")
+            playingCanvas.itemconfig(labelTag, text ="P" + str(playerMark) + " Thinking...", fill=piecesColor[playerMark-1], state=tk.NORMAL)
+            playingWindow.update()
+            col, minmaxScore = connectFour.minimax(5, -math.inf, math.inf, True)
+            addPiece(col, piecesColor[playerMark-1])
+            connectFour.connectFourBoard.placePiece(col, playerMark)
+            playingCanvas.itemconfig(labelTag, state=tk.HIDDEN)
+            if connectFour.isWinMovement(playerMark):
+                playing = False
+                playAgain = messagebox.askyesno("Connect four", "Player" + str(playerMark) +   " (Computer) WON.\nDo you want to play again?")
+                if playAgain == True:
+                    resetAndPlayAgain()
+                else:
+                    sys.exit(0)
+        playingWindow.update()
+        playingWindow.update_idletasks()
+        
+        
 
 
 def mousePressed(event):
     global turn
-    if turn == "player1":
+    global playerMark
+    global playing
+    if turn == "Human":
         x = event.x
         col = int(x/(WIDTH/NCOL))
-        addPiece(col, "green")
+        addPiece(col, piecesColor[playerMark-1])
+        connectFour.connectFourBoard.placePiece(col, playerMark)
+        if connectFour.isWinMovement(playerMark):
+            playing = False
+            playAgain = messagebox.askyesno("Connect four", "Player" + str(playerMark) + " (Human) WON.\nDo you want to play again?")
+            if playAgain == True:
+                resetAndPlayAgain()
+            else:
+                sys.exit(0)
 
 def addPiece(col, color):
     for i in range(NROWS):
@@ -88,11 +153,26 @@ def addPiece(col, color):
 
 def switchTurns():
     global turn
-    if turn == "player1":
-        turn = "compupter"
-    elif turn == "computer":
-        turn = "player1"
+    global playerMark
+    global playingMode
+    playerMark = (playerMark%2) + 1
+    if playingMode == "HvsC":
+        if turn == "Human":
+            turn = "Computer"
+        elif turn == "Computer":
+            turn = "Human"
+
+def resetAndPlayAgain():
+    global playingCanvas
+    global connectFour
+    if playingCanvas != None:
+        playingCanvas.destroy()
+    if connectFour != None:
+        del(connectFour)
+    playingWindow.withdraw()
+    playingModeWindow.deiconify()
+    playingModeWindow.focus()
         
 
-
-init()
+if __name__ == "__main__":
+    init()
